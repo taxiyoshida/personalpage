0eeb0f1b22ea1a2053acd7bdacc58608be5ea1ed
Yoshi <yoshi@axion.zone>
2018-09-12 14:25:51 +0900

    Initialize repository

---
+++ src/pages/index.js
@@ -0,0 +1,34 @@
+import React from 'react'
+
+const IndexPage = () => (
+  <div>
+    <h1 className="fw5 sans-serif">Yoshi 吉田拓史</h1>
+    <p>Developing business news app, but stucked I need help, engineer. </p>
+    <p>
+<h3>Profile</h3>
+<ul>
+<li><a href="https://www.linkedin.com/in/takushi-yoshida-a7749961/">LinkedIn</a>[English]</li>
+<li><a href="https://twitter.com/taxiyoshida">Twitter</a>[Mainly Japanese]</li>
+</ul>
+<h3>Weblog</h3>
+<ul>
+<li><a href="https://taxi-yoshida.hatenablog.com/">デジタルエコノミー研究所</a>[Japanese]</li>
+<li><a href="https://medium.com/@taxiyoshida">Medium</a>[English]</li>
+</ul>
+<h3>Music</h3>
+<ul>
+<li><a href="https://soundcloud.com/alcoholic-vivi">Alcoholic Vivi</a></li>
+<li><a href="https://soundcloud.com/taxi_yoshida">Takushi_Yoshida</a> </li>
+</ul>
+<h3>Novels</h3>
+<ul>
+<li><a href="https://note.mu/kei_natsukawa/m/m3eb6baa54801">月の恋人[2015年5月製作]</a></li>
+<li><a href="https://note.mu/kei_natsukawa/m/mc55ff65e5441">カオスが見た夢[2015年2月製作]</a></li>
+<li><a href="https://note.mu/kei_natsukawa/m/m30e2a27dc98b">未来はいま深い海のなかを潜っている[2013年製作]</a></li>
+</ul>
+    </p>
+    
+  </div>
+)
+
+export default IndexPage
